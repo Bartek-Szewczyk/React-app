ad6b04d40a7147f07fda4576906272c2384137ea
Bartek-Szewczyk <ba20001000@gmail.com>
2021-03-20 15:35:24 +0100

    add leftmenu

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -3,18 +3,27 @@ import styled from 'styled-components'
 
 
 import {Navbar} from '../Navbar/Navbar';
+import {LeftMenu} from '../LeftMenu/LeftMenu';
 
 const Wrapper = styled.div`
 `;
 
-
+const Content = styled.div`
+  max-width: 1200px;
+  align-items: center;
+  display: flex;
+  
+`;
 
 
 const App:FC =()=>{
   return(
     <Wrapper>
     <Navbar/>
-      <Wrapper></Wrapper>
+      <Content>
+        <LeftMenu/>
+        <div>content</div>
+      </Content>
     
     </Wrapper>
     
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,19 +7,38 @@ import {Colors} from '../../styledHelpers/Colors';
 //import {ExpandedMenu} from "./ExpandedMenu";
 
 const InnerWrapper = styled.div`
-    width: 1200px;
-    background: ${Colors.black}
+    width: 100%;
+    max-height: 50px;
+    background: ${Colors.white};
+    display: inline-flex;
+    padding: 5px;
+    
 `;
-
+const RightIcon = styled.div`
+    
+    left: 30px
+`;
+const InputWrapper = styled.div`
+    justify-content: center;
+`;
 
 export const Navbar: FC =()=>{
     return(
         <Wrapper>
             <InnerWrapper>
-                <img src="./Media/logo.png"/>
+                <img src="./Media/logo.png" alt=""/>
                 <div>
             
                 </div>
+                <InputWrapper>
+                    <input type="text"/>
+                    <img src="./Media/icons/search.png" alt=""/>
+                </InputWrapper>
+                <RightIcon>
+                    <img src="./Media/icons/house.png" alt=""/>
+                    <img src="./Media/icons/comments.png" alt=""/>
+                    <img src="./Media/icons/bell.png" alt=""/>
+                </RightIcon>
             </InnerWrapper>
         </Wrapper>
     )
