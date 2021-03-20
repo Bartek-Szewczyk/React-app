285775f2a087bd51c28c28f155dd9a22a63c8d99
Bartek-Szewczyk <ba20001000@gmail.com>
2021-03-20 15:35:09 +0100

    add leftmenu

--- src/components/LeftMenu/LeftMenu.tsx
+++ src/components/LeftMenu/LeftMenu.tsx
@@ -3,3 +3,26 @@ import styled from 'styled-components';
 
 import { Wrapper } from '../../styledHelpers/Components';
 import {Colors} from '../../styledHelpers/Colors';
+
+
+const InnerWrapper = styled.div`
+    width: 20%;
+    
+`;
+
+
+
+export const LeftMenu: FC =()=>{
+    return(
+        <Wrapper>
+            <InnerWrapper>
+                <img src="./Media/logo.png" alt=""/>
+                <div>
+            
+                </div>
+                
+                
+            </InnerWrapper>
+        </Wrapper>
+    )
+}
