13b172bae0a47be6f2d12d28410e1964d184ccb1
Bartek-Szewczyk <ba20001000@gmail.com>
2021-04-10 21:33:19 +0200

    add innerwrap in mainpage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -13,8 +13,17 @@ import {
   useLocation,
 } from "react-router-dom";
 const Wrapper = styled.div`
-    
+  width:100vw;
+  text-align: center;
 `;
+
+const InnerWrapper = styled.div`
+  padding: 5px;
+  max-width: 1500px;
+  margin-left:auto;
+  margin-right:auto
+`;
+
 
 const Content = styled.div`
   max-width: 1200px;
@@ -33,34 +42,37 @@ const App:FC =()=>{
 
  return(
     <Router>
+      
       <Wrapper>
         <Navbar user={User}/>
-        <Content>
-          <LeftMenu user={User}/>
-         
-          <Switch>
-            <Route path="/publications">
-              <Publications/>
-            </Route>
-            <Route path="/people">
-              people content
-            </Route>
-            <Route path="/entities">
-              entities content
-            </Route>
-            <Route path="/administration">
-              Administration content
-            </Route>
-            <Route path="/ecosystem">
-              Ecosystem content
-            </Route>
-            <Route path="/">
-              
-            </Route>
-          </Switch>
-        </Content>
+        <InnerWrapper>
+          
+          <Content>
+            <LeftMenu user={User}/>
+          
+            <Switch>
+              <Route path="/publications">
+                <Publications/>
+              </Route>
+              <Route path="/people">
+                people content
+              </Route>
+              <Route path="/entities">
+                entities content
+              </Route>
+              <Route path="/administration">
+                Administration content
+              </Route>
+              <Route path="/ecosystem">
+                Ecosystem content
+              </Route>
+              <Route path="/">
+                
+              </Route>
+            </Switch>
+          </Content>
 
-
+        </InnerWrapper>
       </Wrapper>
     </Router>
   );
